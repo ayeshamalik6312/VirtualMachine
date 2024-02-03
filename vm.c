d4cff027e47557fa132a41245173099c7e8c689b
ayeshamalik6312 <145179838+ayeshamalik6312@users.noreply.github.com>
2024-02-02 20:39:21 -0500

    final version vm.c

--- vm.c
+++ vm.c
@@ -14,13 +14,9 @@ typedef struct InstructionRegister {
     int OP;
     int L;
     int M;
+    char opName[10];
 }InstructionRegister; 
 
-// Questions for Monty:
-// should instruction register be a struct or seperately stored integers
-// can we use global variables (is SP = 500 ok or should it be SP = BP + 1)
-// For 'output file' does everything need to be stored in an actual output file 
-// or just printed to terminal
 
 // Base function. Given in program assignment details
 int base( int BP, int L) {
@@ -35,13 +31,14 @@ int base( int BP, int L) {
 
 int main(int argc, char* argv[]) {
     // File.IO stuff
-    // Need to change this to reading it in as a command line argument not how it is 
+
+    // file input
     char fileName[50]; 
     for (int i = 0; i < argc; i++) { 
         strcpy(fileName, argv[i]); 
     }
     
-    
+    // file management
     int index = 0;
     FILE *file; 
     file = fopen(fileName, "r");
@@ -51,39 +48,120 @@ int main(int argc, char* argv[]) {
     }
     fclose(file);
 
-    
-    printf("                        PC   BP   SP    stack\n");
-    printf("INITIAL VALUES:         %-3d  %-3d  %-3d\n",PC,BP,SP);
+    printf("                 PC   BP   SP    stack\n");
+    printf("INITIAL VALUES:  %-3d  %-3d  %-3d\n",PC,BP,SP);
     // Instruction Cycle
-    int flag = -1; 
+    int flag = -1, ctr = 0; 
     int testCount = 0;
     InstructionRegister IR;
     IR.OP = 0;
     IR.L = 0;
     IR.M = 0; 
+
+
     while (flag != 0) {
         // Fetch
-        printf("    %-3d %-3d %-3d         %-3d  %-3d  %-3d ",IR.OP,IR.L,IR.M,PC,BP,SP);
-       
+        if (ctr > 0) { 
+            printf("    %s   %-3d %-3d %-3d  %-3d  %-3d ",IR.opName,IR.L,IR.M,PC,BP,SP);
+        }
+        else { 
+            printf(" ");
+            ctr++; 
+        }
+
+        // printing stack 
         for (int i = ARRAY_SIZE - 1; i >= SP; i--)
         {
-            if (IR.OP == 5)
+            if (i == BP && i < ARRAY_SIZE - 4)
             {
-                /* code */
+                printf("|"); 
             }
-            
             printf("  %d ",pas[i]);
-        }
+        }     
         printf("\n");
         
-        
+        // assigning AR Variables 
         //testCount++;
         IR.OP = pas[PC]; 
         IR.L = pas[PC + 1]; 
         IR.M = pas[PC + 2];  
         PC += 3; 
-
-        // Execute 
+        // assigning the name of the opcode so its easier to print 
+        switch(IR.OP) {
+            case 1:
+                strcpy(IR.opName, "LIT");
+                break;
+            case 2: 
+                if (IR.M == 0) { 
+                    strcpy(IR.opName, "RTN"); 
+                } 
+                if (IR.M == 1) {
+                    strcpy(IR.opName, "ADD"); 
+                }
+                if (IR.M == 2) {
+                    strcpy(IR.opName, "SUB");
+                }
+                if (IR.M == 3) {
+                    strcpy(IR.opName, "MUL");
+                }
+                if (IR.M == 4) {
+                    strcpy(IR.opName, "DIV");
+                }
+                if (IR.M == 5) {
+                    strcpy(IR.opName, "EQ");
+                }
+                if (IR.M == 6) {
+                    strcpy(IR.opName, "NEQ");
+                }
+                if (IR.M == 7) {
+                    strcpy(IR.opName, "LSS");
+                }
+                if (IR.M == 8) {
+                    strcpy(IR.opName, "LEQ");
+                }
+                if (IR.M == 9) {
+                    strcpy(IR.opName, "GTR");
+                }
+                if (IR.M == 10){
+                    strcpy(IR.opName, "GEQ");
+                }
+                if (IR.M == 11){
+                    strcpy(IR.opName, "ODD");
+                }  
+                break;
+            case 3:
+                strcpy(IR.opName, "LOD");
+                break;
+            case 4:
+                strcpy(IR.opName, "STO");
+                break;
+            case 5:
+                strcpy(IR.opName, "CAL");
+                break;
+            case 6:
+                strcpy(IR.opName, "INC");
+                break;
+            case 7:
+                strcpy(IR.opName, "JMP");
+                break;
+            case 8:
+                strcpy(IR.opName, "JPC");
+                break;
+            case 9:
+                if (IR.M == 1){
+                    strcpy(IR.opName, "SOU");
+                }
+                if (IR.M == 2) {
+                    strcpy(IR.opName, "SIN");
+                }
+                if (IR.M == 3) {
+                    strcpy(IR.opName, "EOP");
+                }
+            break;  
+        }
+     
+        
+        // Execute Cycle
         if (IR.OP == 1) { 
             //LIT OPERATION
             SP--;
@@ -97,74 +175,62 @@ int main(int argc, char* argv[]) {
                 BP = pas[SP - 2];
                 PC = pas[SP - 3];
             }
-
             if (IR.M == 1)//ADD OP
             {
                 pas[SP + 1] = pas[SP + 1] + pas[SP];
                 SP++; 
             }
-
             if (IR.M == 2)//SUB OP
             {
                 pas[SP + 1] = pas[SP + 1] - pas[SP];
                 SP++;
             }
-
             if (IR.M == 3)//MUL OP
             {
                 pas[SP + 1] = pas[SP + 1] * pas[SP];
                 SP++;
             }
-
             if (IR.M == 4)//DIV OP
             {
                 pas[SP + 1] = pas[SP + 1] / pas[SP];
                 SP++;
             }
-
             if (IR.M == 5)//EQL OP
             {
                 pas[SP + 1] = pas[SP + 1] == pas[SP];
                 SP++;
             }
-
             if (IR.M == 6)//NEQ OP
             {
                 pas[SP + 1] = pas[SP + 1] != pas[SP];
                 SP++;
             }
-
             if (IR.M == 7)//LSS OP
             {
                 pas[SP + 1] = pas[SP + 1] < pas[SP];
                 SP++;
             }
-
             if (IR.M == 8)//LEQ OP
             {
                 pas[SP + 1] = pas[SP + 1] <= pas[SP];
                 SP++;
             }
-
             if (IR.M == 9)//GTR OP
             {
                 pas[SP + 1] = pas[SP + 1] > pas[SP];
                 SP++;
             }
-
             if (IR.M == 10)//GEQ OP
             {
                 pas[SP + 1] = pas[SP + 1] >= pas[SP];
                 SP++;
             }
-
             if (IR.M == 11)//ODD OP
             {
                 pas[SP] = pas[SP] % 2;
-            }
-            
-
-        }else if (IR.OP == 3) { 
+            } 
+        } 
+        else if (IR.OP == 3) { 
             // LOD OPERATION
             SP--;
             pas[SP] = pas[base(BP,IR.L) - IR.M];
@@ -194,11 +260,9 @@ int main(int argc, char* argv[]) {
             // JPC OPERATION
             if (pas[SP] == 0)
             {
-                PC = IR.M;
-                
+                PC = IR.M;   
             }
-            SP++;
-            
+            SP++;      
         }
         else if (IR.OP == 9) { 
             // SYS OPERATIONS
@@ -220,23 +284,12 @@ int main(int argc, char* argv[]) {
             if (IR.M == 3)
             {
                 flag = 0;
-                printf("    %-3d %-3d %-3d         %-3d  %-3d  %-3d ",IR.OP,IR.L,IR.M,PC,BP,SP);
+                printf("    %s   %-3d %-3d %-3d  %-3d  %-3d ",IR.opName,IR.L,IR.M,PC,BP,SP);
                 for (int i = ARRAY_SIZE - 1; i >= SP; i--){
                     printf("  %d ",pas[i]);
                 }
-
-            }
-            
-        }
-        
-        
-    }
-
-    // After the fetch-execute cycle is done we need to output it
-    
+            }   
+        }      
+    }    
     return 0;
 }
-
-
-
-
